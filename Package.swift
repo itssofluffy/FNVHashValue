8f555fa5de207aee0a1579c71f5b706755636146
Steve Whittle <steve.whittle@protonmail.com>
2017-03-17 22:07:55 +0000

    now compiles as a shared object

--- Package.swift
+++ Package.swift
@@ -3,9 +3,21 @@ import PackageDescription
 let package = Package(
     name: "FNVHashValue",
     dependencies: [
-        .Package (
-            url:          "https://github.com/itssofluffy/ISFLibrary.git",
-            majorVersion: 0
-        )
+        .Package (url: "https://github.com/itssofluffy/ISFLibrary.git", majorVersion: 0)
     ]
 )
+
+let staticLibrary = Product(
+    name:    "FNVHashValue",
+    type:    .Library(.Static),
+    modules: ["FNVHashValue"]
+)
+
+let dynamicLibrary = Product(
+    name:    "FNVHashValue",
+    type:    .Library(.Dynamic),
+    modules: ["FNVHashValue"]
+)
+
+products.append(staticLibrary)
+products.append(dynamicLibrary)
